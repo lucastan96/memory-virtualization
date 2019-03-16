28aa51ddd9d89d22bf44774932b3c06aa7330a97
Lucas Tan <lucastan96@gmail.com>
2019-03-16 17:08:05 +0000

    Output frame number to page_table.txt

--- lib/util.c
+++ lib/util.c
@@ -31,6 +31,9 @@ void write_files(char *address_space)
     FILE *file_physical_memory = fopen("./data/physical_memory.txt", "w");
     FILE *file_page_table = fopen("./data/page_table.txt", "w");
 
+    int starting_frame = 0;
+    int ending_frame = 0;
+
     if (file_physical_memory == NULL)
     {
         printf("Error: Failed to open physical memory text file.");
@@ -50,6 +53,11 @@ void write_files(char *address_space)
             else
             {
                 fprintf(file_physical_memory, "0x0%05x  |%-3d       |%c         |%d\n", i, frame, address_space[i], 1);
+                if (starting_frame == 0)
+                {
+                    starting_frame = frame;
+                }
+                ending_frame = frame - 1;
             }
         }
     }
@@ -66,7 +74,14 @@ void write_files(char *address_space)
         fprintf(file_page_table, "----------|----------\n");
         for (int i = 0; i < PAGE_SIZE; ++i)
         {
-            fprintf(file_page_table, "0x0%02x     |\n", i);
+            if (i < ending_frame)
+            {
+                fprintf(file_page_table, "0x0%02x     |%d\n", i, starting_frame + i);
+            }
+            else
+            {
+                fprintf(file_page_table, "0x0%02x     |\n", i);
+            }
         }
     }
 }
