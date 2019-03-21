cdd4e2abf10c15f9d216ab2f96ce17ec4007e728
Lucas Tan <lucastan96@gmail.com>
2019-03-21 16:31:14 +0000

    Added assigning frame number and present bit to address space

--- lib/util.c
+++ lib/util.c
@@ -7,10 +7,10 @@
 
 int generate_random_num(int min, int max)
 {
-    return (rand() % (max - min)) + min;
+    return (rand() % (max + 1 - min)) + min;
 }
 
-char generate_random_char()
+unsigned char generate_random_char()
 {
     const int CHAR_MAX = 126;
     const int CHAR_MIN = 33;
@@ -18,28 +18,35 @@ char generate_random_char()
     return (rand() % (CHAR_MAX - CHAR_MIN)) + CHAR_MIN;
 }
 
-void populate_address_space(char *address_space)
+void populate_address_space(unsigned char *address_space)
 {
     int starting_address = generate_random_num(PAGE_SIZE * 2, ADDRESS_SPACE_SIZE);
     int num_of_population = generate_random_num(2048, 20480);
+    int ending_address = starting_address + num_of_population;
 
-    if (starting_address + num_of_population > ADDRESS_SPACE_SIZE)
+    if (ending_address > ADDRESS_SPACE_SIZE)
     {
-        starting_address = starting_address - (ADDRESS_SPACE_SIZE - starting_address + num_of_population);
+        starting_address = starting_address - (ADDRESS_SPACE_SIZE - ending_address);
     }
 
-    for (int i = starting_address; i < starting_address + num_of_population; ++i)
+    for (int i = starting_address; i < ending_address; ++i)
     {
         address_space[i] = generate_random_char();
     }
+
+    int num_of_frames = (ending_address - starting_address) / PAGE_SIZE;
+    int starting_frame = starting_address / PAGE_SIZE;
+
+    for (int i = 0; i <= num_of_frames; ++i)
+    {
+        address_space[i] = (starting_frame + i);
+        address_space[i + PAGE_SIZE] = 1;
+    }
 }
 
-void write_files(char *address_space)
+void write_files(unsigned char *address_space)
 {
     FILE *file_physical_memory = fopen("./data/physical_memory.txt", "w");
-
-    int starting_frame = 0;
-    int ending_frame = 0;
 
     if (file_physical_memory == NULL)
     {
@@ -53,18 +60,20 @@ void write_files(char *address_space)
         for (int i = 0; i < ADDRESS_SPACE_SIZE; ++i)
         {
             frame = i / PAGE_SIZE;
-            if (address_space[i] == 0)
+            if (i < PAGE_SIZE * 2)
             {
-                fprintf(file_physical_memory, "0x0%05x  |%-3d       |          |%d\n", i, frame, 0);
+                fprintf(file_physical_memory, "0x0%05x  |%-3d       |          |%d\n", i, frame, 1);
             }
             else
             {
-                fprintf(file_physical_memory, "0x0%05x  |%-3d       |%c         |%d\n", i, frame, address_space[i], 1);
-                if (starting_frame == 0)
+                if (address_space[i] == 0)
                 {
-                    starting_frame = frame;
+                    fprintf(file_physical_memory, "0x0%05x  |%-3d       |          |%d\n", i, frame, 0);
                 }
-                ending_frame = frame;
+                else
+                {
+                    fprintf(file_physical_memory, "0x0%05x  |%-3d       |%c         |%d\n", i, frame, address_space[i], 1);
+                }
             }
         }
     }
@@ -72,11 +81,6 @@ void write_files(char *address_space)
     fclose(file_physical_memory);
 
     FILE *file_page_table = fopen("./data/page_table.txt", "w");
-
-    printf("start: %d\n", starting_frame);
-    printf("end: %d\n", ending_frame);
-
-    int present_bit = 1;
 
     if (file_page_table == NULL)
     {
@@ -88,22 +92,24 @@ void write_files(char *address_space)
         fprintf(file_page_table, "----------|----------|------------\n");
         for (int i = 0; i < PAGE_SIZE; ++i)
         {
-            if (i <= ending_frame - starting_frame)
+            if (address_space[i] != 0)
             {
-                fprintf(file_page_table, "0x0%02x     |%-3d       |%d\n", i, starting_frame + i, present_bit);
+                fprintf(file_page_table, "0x0%02x     |%-3d       |%d\n", i, address_space[i], address_space[i + 256]);
             }
             else
             {
-                fprintf(file_page_table, "0x0%02x     |          |%d\n", i, present_bit);
+                fprintf(file_page_table, "0x0%02x     |          |%d\n", i, 0);
             }
         }
     }
+
+    fclose(file_page_table);
 }
 
 void run()
 {
     srand(time(NULL));
-    char *address_space = malloc(ADDRESS_SPACE_SIZE);
+    unsigned char *address_space = malloc(ADDRESS_SPACE_SIZE);
     populate_address_space(address_space);
     write_files(address_space);
 }
